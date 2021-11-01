6ec252a84e240cdacb17af33b4a155ff2ec91b02
OwenMille <90118261+OwenMille@users.noreply.github.com>
2021-11-01 17:42:29 -0400

    Add profile page

--- src/js/components/profile/ProfilePage.tsx
+++ src/js/components/profile/ProfilePage.tsx
@@ -1,7 +1,59 @@
-export function Profile() {
-    return(
-        <h1>test</h1>
+import React  from 'react';
+//@ts-ignore
+import DefaultUserPic from "../../../assets/images/DefaultUserPic.jpeg";
+
+
+require("../../../style/profile/ProfilePage.scss");
+
+/* interface userprofile {
+    user_id: string[];
+    username?: string[];
+    email?: string[];
+    profileImage?: string;
+    msg?:string[];
+}
+interface userprofileState{
+    user_id: string[];
+    username?: string[];
+    email?: string[];
+    profileImage?: string;
+    msg?:string[];
+} */
+export function UserProfile(){
+    /* constructor(props: any){
+        super(props);
+        this.state={
+            user_id:this.props.user_id,
+            username:this.props.username,
+            email:this.props.email,
+            profileImage:this.props.profileImage,
+            msg:this.props.msg,
+        }
+    }
+render(){
+
+    if(this.state.profileImage){
+        var profilePic=this.state.profileImage;
+    }else{
+         profilePic=DefaultUserPic;
+    } */
+    return (
+        <div className="profile-container">
+            <div className="profile-hero">
+                <img src={DefaultUserPic} alt="profile pic"/>
+                <div className="details-container">
+                    <header>
+                        <p>@owenmille</p>
+                        <button>3 <small>posts</small></button>
+                        <button>9 <small>books</small></button>
+                        <h1>Owen Miller</h1>
+                    </header>   
+                    <div>
+                        <p>Hello everyone! just made my account so feel free to add me as a friend :)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-
-export default Profile
+   export default UserProfile
