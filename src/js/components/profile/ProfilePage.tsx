408ceb3f4acfffd4c2adf4d57d0cbffee4def6f7
OwenMille <90118261+OwenMille@users.noreply.github.com>
2021-11-18 15:12:29 -0500

    Postfeed (#29)
    * starting redux
    
    * temp
    
    * owen_miller_homeworkfour
    
    * temp
    
    * Add Redux store and a working (beginner) functionality for posting and post feed

--- src/js/components/profile/ProfilePage.tsx
+++ src/js/components/profile/ProfilePage.tsx
@@ -1,42 +1,11 @@
 import React  from 'react';
 //@ts-ignore
 import DefaultUserPic from "../../../assets/images/DefaultUserPic.jpeg";
-
+import PostFeed from '../posts/PostFeed'
 
 require("../../../style/profile/ProfilePage.scss");
 
-/* interface userprofile {
-    user_id: string[];
-    username?: string[];
-    email?: string[];
-    profileImage?: string;
-    msg?:string[];
-}
-interface userprofileState{
-    user_id: string[];
-    username?: string[];
-    email?: string[];
-    profileImage?: string;
-    msg?:string[];
-} */
 export function UserProfile(){
-    /* constructor(props: any){
-        super(props);
-        this.state={
-            user_id:this.props.user_id,
-            username:this.props.username,
-            email:this.props.email,
-            profileImage:this.props.profileImage,
-            msg:this.props.msg,
-        }
-    }
-render(){
-
-    if(this.state.profileImage){
-        var profilePic=this.state.profileImage;
-    }else{
-         profilePic=DefaultUserPic;
-    } */
     return (
         <div className="profile-container">
             <div className="profile-hero">
@@ -51,6 +20,12 @@ render(){
                     <div>
                         <p>Hello everyone! just made my account so feel free to add me as a friend :)</p>
                     </div>
+                </div>
+                  <div className="profile-bookshelf">
+                </div>
+                <div className="posts-container">
+                    <h1>Recent Posts</h1>
+                    <PostFeed/>
                 </div>
             </div>
         </div>
---
+++ src/js/store/actions/actionTypes.ts
@@ -0,0 +1,13 @@
+
+export const ADD_POST = 'ADD_POST';
+export const GET_POST = 'GET_POSTS';
+export const GET_POSTS = 'GET_POSTS';
+export const REMOVE_POST = 'REMOVE_POST';
+
+export const POST_ERROR = 'POST_ERROR';
+
+export const GET_PROFILE = 'GET_PROFILE';
+export const GET_PROFILES = 'GET_PROFILES';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
