1c839fb810ed27406be6327c47bb0b9d290fd45b
Owen Miller <cp8379@wayne.edu>
2021-11-26 17:32:46 -0500

    add client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -23,3 +23,26 @@ export function authenticatedHttpGet(url: string) {
         console.error(error);
     })
   }
+
+export function authenticatedHttpPost(url: string) {
+    
+    const user: QuickLitUser = getUser();
+    if(!user){
+        return NOT_SIGNED_IN_RESPONSE;
+    }
+    const request: Request = new Request(url);
+    console.error(`user is ${JSON.stringify(user)}`)
+    request.headers.append("Authorization", user.cognitoTokenJWT);
+    
+    return fetch(request).then((response)=>{
+        const status: number = response.status as number;
+        if (status >= 200 && status <= 399) {
+            const responseJson = response.json();
+            console.log(responseJson)
+            return responseJson;
+        }
+        return Promise.reject(response);
+    }).catch((error) =>{
+        console.error(error);
+    })
+  }
